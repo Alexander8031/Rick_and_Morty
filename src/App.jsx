4171c0266d9ba5273b3d1480d729f2049476dfcb
Alexander8031 <Alex_vasilev8031@bk.ru>
2024-11-08 08:40:32 +0300

    1/Отредактированы архитектура и комментарии

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,9 @@
-import { Routes, Route } from "react-router-dom";
-import { Layout } from "./Layout";
 import "./App.css";
-import Button from "./components/Button/Button";
+
 
 function App() {
   return (
     <>
-      <Button type="primary" onClick={() => console.log("Check")}>
-        LOAD MORE
-      </Button>
-      <Button type="secondary" onClick={() => console.log("CheckOne")}>
-        Characters
-      </Button>
-      <Button type="secondary" onClick={() => console.log("CheckTwo")}>
-        Locations
-      </Button>
-      <Button type="secondary" onClick={() => console.log("CheckThree")}>
-        Episodes
-      </Button>
-      {/* <Routes>
-        <Route path="/" element={<Layout />}>
-          {<Route index element={<Home />} />
-          <Route path="counter" element={<Counter />} />
-          <Route path="input" element={<Input />} /> }
-        </Route>
-      </Routes> */}
     </>
   );
 }
--- src/Layout.jsx
+++ src/Layout.jsx
@@ -3,11 +3,6 @@ import { Link, Outlet } from "react-router-dom";
 const Layout = () => {
     return (
       <>
-        {/* <Header>
-          <Link to="/">Home</Link>
-          <Link to="/counter">Counter</Link>
-          <Link to="/input">Input</Link>
-        </Header> */}
   
         <Outlet />
       </>
