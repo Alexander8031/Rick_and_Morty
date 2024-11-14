9d19a42f680b306dfa5e4d3619752b1b219ca974
Alexander8031 <Alex_vasilev8031@bk.ru>
2024-11-14 21:14:19 +0300

    4/Скорректирована компонента селекта

--- src/components/Select/select.jsx
+++ src/components/Select/select.jsx
@@ -1,10 +1,11 @@
 import classes from "../Select/select.module.css";
 import { useState } from "react";
+import { ReactComponent as MiniArrow } from "../../../public/image/icons/miniArrow.svg";
 
-export default function Select({ id, name, selectOptions }) {
+export default function Select({ id, name, options }) {
   const [open, setOpen] = useState(false);
 
-  const handleMouseDown = () => {
+  const handleOnClick = () => {
     setOpen((previousState) => !previousState);
   };
 
@@ -14,23 +15,27 @@ export default function Select({ id, name, selectOptions }) {
 
   return (
     <div className={`${classes.wrapper} ${open ? classes.open : ""}`}>
-      <select
-        id={id}
-        name={name}
-        className={classes.select}
-        onMouseDown={handleMouseDown}
-        onBlur={handleBlur}
-        defaultValue=""
-      >
-        <option value="" disabled hidden>{name}</option>
-        {selectOptions &&
-          selectOptions.map((option, index) => (
-            <option key={index} value={option}>
-              {option}
-            </option>
-          ))}
-      </select>
-      <img src="./public/image/icons/mini-arrow.svg" alt="▾" className={classes.arrow} />
+      <div className={classes.selectContainer}>
+        <select
+          id={id}
+          name={name}
+          className={classes.select}
+          onClick={handleOnClick}
+          onBlur={handleBlur}
+          defaultValue=""
+        >
+          <option value="" disabled hidden>
+            {name}
+          </option>
+          {options &&
+            options.map((option, index) => (
+              <option key={index} value={option}>
+                {option}
+              </option>
+            ))}
+        </select>
+        <MiniArrow alt="▾" className={classes.arrow} />
+      </div>
     </div>
   );
 }
