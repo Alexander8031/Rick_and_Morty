ed3e5386e7cfe4850d2b4dcb800ec38d28cbca2c
Alexander8031 <Alex_vasilev8031@bk.ru>
2024-11-18 16:01:43 +0300

    4/Переработана компонента селекта, headlessui -> listbox

---
+++ src/components/Selects/selects.jsx
@@ -0,0 +1,34 @@
+import classes from "./selects.module.css";
+import {
+  Listbox,
+  ListboxButton,
+  ListboxOption,
+  ListboxOptions,
+} from "@headlessui/react";
+import { ReactComponent as MiniArrow } from "../../../public/image/icons/miniArrow.svg";
+
+export default function Selects({ placeholder, name, options }) {
+  return (
+    <Listbox as="div" name={name} className={classes.wrapper}>
+      {({ value }) => (
+        <>
+          <ListboxButton className={classes.select}>
+            <span className={classes.selectText}>{value || placeholder}</span>
+            <MiniArrow alt="▾" className={classes.arrow} />
+          </ListboxButton>
+          <ListboxOptions className={classes.options}>
+            {options.map((option, index) => (
+              <ListboxOption
+                key={index}
+                value={option}
+                className={classes.option}
+              >
+                <span>{option}</span>
+              </ListboxOption>
+            ))}
+          </ListboxOptions>
+        </>
+      )}
+    </Listbox>
+  );
+}
