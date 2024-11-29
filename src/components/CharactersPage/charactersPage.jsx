1eb21f4ca5a3f65b525e42ab565ed9989980e950
Alexander8031 <Alex_vasilev8031@bk.ru>
2024-11-29 13:16:03 +0300

    8/Скорректирована компонента страницы с персонажами и обновлены стили input и Selects

--- src/components/CharactersPage/charactersPage.jsx
+++ src/components/CharactersPage/charactersPage.jsx
@@ -1,39 +1,55 @@
 import classes from "../CharactersPage/charactersPage.module.css";
+import { useState } from "react";
 import { ReactComponent as RickAndMorty } from "../../../public/icons/RickAndMorty.svg";
 import Input from "../input/input";
 import CharacterCard from "../characterCard/characterCard";
 import Button from "../Button/Button";
+import useFetching from "../UseFetching/UseFetching";
+import Selects from "../Selects/selects";
 
 export default function CharactersPage() {
-  const { data, isLoading, error } = useFetching("/character")  
+  const { data, isLoading, error } = useFetching("/character");
+  const [ visibleCount, setVisibleCount ] = useState(8);
 
   if (isLoading) {
-    return <p>Загружаем персонажей...</p>
+    return <p>Загружаем персонажей...</p>;
   }
 
   if (error) {
-    return <p>Ошибка: {error}</p>
+    return <p>Ошибка: {error}</p>;
   }
- 
+
+  const visibleCharacters = data?.results?.slice(0, visibleCount);
+  const handleLoadMore = () => {
+    setVisibleCount((prevCount) => prevCount + 8);
+  };
+
   return (
-    <>
+    <div>
       <div className={classes.containerImg}>
         <RickAndMorty className={classes.img} />
       </div>
       <div className={classes.containerInputSelects}>
         <Input type="search" />
+        {/* <Selects placeholder="Species"/> */}
+        {/* <Selects placeholder="Gender"/> */}
+        {/* <Selects placeholder="Status"/> */}
         <Input type="search" />
         <Input type="search" />
         <Input type="search" />
       </div>
       <div className={classes.containerCharacters}>
-        {data.restults.map((character) => (
+        {visibleCharacters?.map((character) => (
           <CharacterCard key={character.id} character={character} />
         ))}
       </div>
       <div className={classes.containerButton}>
-        <Button type="primary">LOAD MORE</Button>
+        {visibleCount < data?.results?.length && (
+          <Button type="primary" onClick={handleLoadMore}>
+            LOAD MORE
+          </Button>
+        )}
       </div>
-    </>
+    </div>
   );
 }
